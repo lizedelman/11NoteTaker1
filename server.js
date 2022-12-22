67a48d490f52ec15ed768d0b139f83539f403066
Wild Carrot <edelman.eliz@gmail.com>
2022-12-22 15:36:28 -0700

    adding html and assoc functions

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 // Helper method for generating unique ids
-const uuid = require("./helpers/uuid");
+const uuid = require("./node_modules/uuid");
+const getAndRenderNotes = require("./public/js/index.js");
+
 
 const PORT = 3001;
 
@@ -14,68 +16,68 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"))
 );
 
-// GET request for reviews
-app.get("/api/reviews", (req, res) => {
+// GET request for notes
+app.get("/api/notes", (req, res) => {
   // Send a message to the client
-  res.status(200).json(`${req.method} request received to get reviews`);
+  res.status(200).json(`${req.method} request received to get notes`);
 
   // Log our request to the terminal
-  console.info(`${req.method} request received to get reviews`);
+  console.info(`${req.method} request received to get notes`);
 });
 
-// POST request to add a review
-app.post("/api/reviews", (req, res) => {
+// POST request to add a note
+app.post("/api/notes", (req, res) => {
   // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
+  console.info(`${req.method} request received to add a note`);
 
   // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
+  const { title, text, note_id } = req.body;
 
   // If all the required properties are present
-  if (product && review && username) {
+  if (title && text) {
     // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      review_id: uuid(),
+    const newNote = {
+      title,
+      text,
+      // note_id: uuid(),
     };
 
-    // Obtain existing reviews
-    fs.readFile("./db/reviews.json", "utf8", (err, data) => {
+    // Obtain existing notes
+    fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
         // Convert string into JSON object
-        const parsedReviews = JSON.parse(data);
+        const parsedNotes = JSON.parse(data);
 
-        // Add a new review
-        parsedReviews.push(newReview);
+        // Add a new note
+        parsedNotes.push(newNote);
 
-        // Write updated reviews back to the file
+        // Write updated notes back to the file
         fs.writeFile(
-          "./db/reviews.json",
-          JSON.stringify(parsedReviews, null, 4),
+          "./develop/db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
-              : console.info("Successfully updated reviews!")
+              : console.info("Successfully updated notes!")
+              getAndRenderNotes();
         );
       }
     });
 
     const response = {
       status: "success",
-      body: newReview,
+      body: newNote,
     };
 
     console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json("Error in posting review");
+    res.status(500).json("Error in posting note");
   }
 });
 

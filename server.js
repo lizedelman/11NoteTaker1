0999206628ba8087573004b22db06d0120d7f75d
Wild Carrot <edelman.eliz@gmail.com>
2022-12-23 14:18:57 -0700

    port lsiten

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const htmlroutes = require("./routes/htmlroutes");
 app.use("/api", apiroutes);
 app.use("/", htmlroutes);
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || "0.0.0.0";
-server.listen(server_port, server_host, function () {
-  console.log("Listening on port %d", server_port);
-});
+app.listen(process.env.PORT || 3001, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);

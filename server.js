7bc14fdb3d721a99316b07c2865049f5d406e784
Wild Carrot <edelman.eliz@gmail.com>
2022-12-23 14:17:35 -0700

    getting port to listen

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const htmlroutes = require("./routes/htmlroutes");
 app.use("/api", apiroutes);
 app.use("/", htmlroutes);
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+server.listen(server_port, server_host, function () {
+  console.log("Listening on port %d", server_port);
+});
